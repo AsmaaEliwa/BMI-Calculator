a17407ec8908e5d5a60ad03b0065fe4d9f47cf60
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-04-30 12:39:37 -0400

    connected the sliders and the value

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var slider1Value: UILabel!
+    
+    @IBOutlet weak var slider2Value: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderChangeValue(_ sender: UISlider) {
+        if sender == slider1{
+//            slider1Value.text =  String(format:"%.2f",sender.value)
+            slider1Value.text =  "\(Int(sender.value)) m"
+        }else{
+//            slider2Value.text =  String(format:"%.2f",sender.value)
+            slider2Value.text =  "\(Int(sender.value)) Kg"
+        }
+    }
+    
 }
 
